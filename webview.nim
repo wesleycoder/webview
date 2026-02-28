3dde4625e7c8405992417713fd385d2037523db7
Wésley Guimarães <wes@guima.dev>
2026-02-28 15:45:26 -0300

    Update webview.nim
    Removing global comment as it is out of context by removing the global pragma

--- webview.nim
+++ webview.nim
@@ -409,9 +409,6 @@ proc bindCallback*(w: Webview; name: string;
                  fn: proc (id: string; req: JsonNode): string): WebviewError {.discardable.} =
   ## Essentially a high-level version of
   ## `webviewBind <#webviewBind,Webview,cstring,proc(cstring,cstring,pointer),pointer>`_
-  
-  #      using global seems to work...
-  # TODO is there a better solution?
   let arg = CallBackContext(w: w, fn: fn)
 
   result = w.webviewBind(name, closure, cast[pointer](arg))
